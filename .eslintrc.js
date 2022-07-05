47c3d45dfc8b7805815edb2c5a75eb322a80ffa5
Paul <skrimorill@mail.ru>
2022-07-05 06:18:14 +0300

    adding dist subtree

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
-  root: true,
   env: {
-    node: true,
+    browser: true,
+    es2021: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'standard'
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  plugins: [
+    'vue'
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'eol-last': 'off',
+    'vue/multi-word-component-names': 0
+  }
+
+}
